b1b848544bb1319346c7f12c4571fed03de82e84
sagar <manesagar100@gmail.com>
2021-08-01 11:34:03 +0530

    first commit

---
+++ Day1.demo/src/main/java/demo/LoginForm.java
@@ -0,0 +1,31 @@
+package demo;
+
+		import org.apache.struts.action.ActionForm;
+		 
+		public class LoginForm extends ActionForm {
+		 
+			private String userName = null;
+			private String password = null;
+		 
+			public String getUserName() {
+				return userName;
+			}
+		 
+			public void setUserName(String userName) {
+				this.userName = userName;
+			}
+		 
+			public String getPassword() {
+				return password;
+			}
+		 
+			public void setPassword(String password) {
+				this.password = password;
+			}
+		 /*
+			@Override
+			public void reset(ActionMapping mapping, HttpServletRequest request) {
+				this.password = null;
+			} */
+		 
+		}
